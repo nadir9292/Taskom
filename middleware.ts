c9972c5b29fd253e88bffa1f96066b3e3ef4121c
Nadir Mansouri <61685527+nadir9292@users.noreply.github.com>
2025-04-08 21:54:46 +0200

    add token2 in middeleware

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,16 @@ import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get('next-auth.session-token')?.value
+  const token2 = request.cookies.get('__Secure-next-auth.session-token')?.value
   const { pathname } = request.nextUrl
 
-  if (!token && pathname !== '/login') {
+  // Si aucun token et que ce n'est pas la page login, rediriger vers /login
+  if (!token && !token2 && pathname !== '/login') {
     return NextResponse.redirect(new URL('/login', request.url))
   }
 
-  if (token && pathname === '/login') {
+  // Si un token est présent et qu'on est sur la page login, rediriger vers la page d'accueil
+  if ((token || token2) && pathname === '/login') {
     return NextResponse.redirect(new URL('/', request.url))
   }
 
