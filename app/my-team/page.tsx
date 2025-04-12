645ff0b9cf0b75840fa7bdb40420d7af7d739c07
Nadir Mansouri <61685527+nadir9292@users.noreply.github.com>
2025-04-12 23:05:30 +0200

    fix some bug

---
+++ app/my-team/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = object
+
+const page = ({}: Props) => {
+  return (
+    <div>
+      <h1 className="text-center animate-bounce">Working progess...</h1>
+    </div>
+  )
+}
+export default page
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,10 @@
 
 import { BackgroundGradient } from '@/src/components/ui/background-gradient'
 import { useApiRoutes } from '@/src/contexts/ApiContext'
-import { UserGroupIcon } from '@heroicons/react/24/outline'
-import { HomeIcon } from '@heroicons/react/24/outline'
 import { signOut, useSession } from 'next-auth/react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 
 const NavBar = () => {
@@ -14,6 +13,7 @@ const NavBar = () => {
   const [mounted, setMounted] = useState(false)
   const { user } = useApiRoutes()
   const { data: session, status } = useSession()
+  const pathname = usePathname()
 
   useEffect(() => {
     setMounted(true)
@@ -31,102 +31,118 @@ const NavBar = () => {
   return (
     <>
       {windowWidth >= 600 ? (
-        <div className="w-[95vw] mt-4 mx-auto">
-          <BackgroundGradient className="navbar rounded-[22px] bg-[#F5F5F5] dark:bg-[#0e1111]">
-            <div className="flex-1">
-              <Link href="/" className="btn btn-ghost text-xl">
-                Taskom
-              </Link>
-            </div>
-            <div className="flex-none">
-              <ul className="menu menu-horizontal items-center text-xs md:text-xl">
-                <button className="btn btn-ghost">My team</button>
-                <li style={{ zIndex: 100 }}>
-                  <div className="dropdown dropdown-end">
-                    <div
-                      tabIndex={0}
-                      role="button"
-                      className="font-bold flex items-center"
-                    >
-                      <Image
-                        src={
-                          user.profileimage
-                            ? user.profileimage
-                            : '/default-profile.png'
-                        }
-                        height={40}
-                        width={40}
-                        alt="profile image"
-                        priority
-                        className="ml-2 rounded-full"
-                      />
+        <>
+          <div></div>
+          <div className="w-[95vw] mt-4 mx-auto border-[#F5F5F5]">
+            <BackgroundGradient className="navbar rounded-[20px] bg-[#F5F5F5] dark:bg-[#0e1111]">
+              <div className="flex-1">
+                <Link href="/" className="btn btn-ghost text-xl">
+                  Taskom
+                </Link>
+              </div>
+              <div className="flex-none">
+                <ul className="menu menu-horizontal items-center text-xs md:text-xl">
+                  <button className="btn btn-ghost">My team</button>
+                  <li style={{ zIndex: 100 }}>
+                    <div className="dropdown dropdown-end">
+                      <div
+                        tabIndex={0}
+                        role="button"
+                        className="font-bold flex items-center"
+                      >
+                        <Image
+                          src={
+                            user.profileimage
+                              ? user.profileimage
+                              : '/default-profile.png'
+                          }
+                          height={40}
+                          width={40}
+                          alt="profile image"
+                          priority
+                          className="ml-2 rounded-full"
+                        />
+                      </div>
+                      <ul
+                        tabIndex={0}
+                        className="dropdown-content menu bg-[#F5F5F5] rounded-box z-1 w-52 p-2 shadow-xl"
+                      >
+                        <li>
+                          <button className="font-bold">My profile</button>
+                        </li>
+                        <li>
+                          <button
+                            onClick={() => signOut()}
+                            className="text-error font-bold"
+                          >
+                            Logout
+                          </button>
+                        </li>
+                      </ul>
                     </div>
-                    <ul
-                      tabIndex={0}
-                      className="dropdown-content menu bg-[#F2F0EF] rounded-box z-1 w-52 p-2 shadow-sm"
-                    >
-                      <li>
-                        <button className="font-bold">My profile</button>
-                      </li>
-                      <li>
-                        <button
-                          onClick={() => signOut()}
-                          className="text-error font-bold"
-                        >
-                          Logout
-                        </button>
-                      </li>
-                    </ul>
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </BackgroundGradient>
-        </div>
+                  </li>
+                </ul>
+              </div>
+            </BackgroundGradient>
+          </div>
+        </>
       ) : (
-        <div className="fixed bottom-4 w-[95vw] mx-auto left-0 right-0">
-          <BackgroundGradient className="dock dock-lg rounded-[22px] bg-[#F5F5F5] dark:bg-[#0e1111]">
-            <button>
-              <UserGroupIcon height={30} />
+        <div className="fixed bottom-6 w-[95vw] mx-auto left-0 right-0">
+          <BackgroundGradient className="dock dock-lg rounded-[20px] bg-[#F5F5F5] border border-[#F5F5F5]">
+            <button
+              className={`${pathname === '/my-team' ? 'dock-active' : ''}`}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z"
+                />
+              </svg>
               <span className="dock-label">My team</span>
             </button>
+            <button className={`${pathname === '/' ? 'dock-active' : ''}`}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                />
+              </svg>
 
-            <button>
-              <HomeIcon height={30} />
               <span className="dock-label">Home</span>
             </button>
-
             <button
-              tabIndex={0}
-              role="button"
-              className="dropdown dropdown-top dropdown-end"
+              className={` ${pathname === '/my-profile' ? 'dock-active' : ''}`}
             >
               <Image
                 src={
-                  user.profileimage ? user.profileimage : '/default-profile.png'
+                  session.user?.image
+                    ? session.user.image
+                    : '/default-profile.png'
                 }
-                height={40}
-                width={40}
-                alt="profile image"
-                priority
-                className="rounded-full border-2 border-black mx-auto"
+                height={24}
+                width={24}
+                className="rounded-full"
+                alt="image profile"
               />
-              <ul
-                tabIndex={0}
-                className="dropdown-content menu bg-[#F2F0EF] rounded-box z-1 w-52 p-2 shadow-sm"
-              >
-                <li>
-                  <div className="font-bold">My profile</div>
-                </li>
-                <li>
-                  <div
-                    onClick={() => signOut()}
-                    className="text-error font-bold"
-                  >
-                    Logout
-                  </div>
-                </li>
-              </ul>
+              <span className="dock-label">
+                {session.user?.name ? session.user.name.split(' ')[0] : ''}
+              </span>
             </button>
           </BackgroundGradient>
         </div>
